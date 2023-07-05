8cbaf8779fa5bdb8a674e0ce0ea16eeedd16d8b1
pcy5653 <pcy7625653@naver.com>
2023-07-05 23:45:30 +0900

    0705_Student_test

--- src/com/iu/study4/s2/S2Main.java
+++ src/com/iu/study4/s2/S2Main.java
@@ -6,12 +6,17 @@ public class S2Main {
 
 		StudentService sv = new StudentService();
 		
-		// makeStudent 메서드
-		Student student = sv.makeStudentOne();
-		System.out.println(student.avg);
+		// makeStudeSnt 메서드
+		//Student student = sv.makeStudentOne();
+		//System.out.println(student.avg);
 		
 		// makeStudents 메서드
-		Student [] st = sv.makeStudents();
+		//Student [] st = sv.makeStudents();
+		
+		// StudentController class 변수 선언
+		StudentController control = new StudentController();
+		// control class에서 start()메서드의 반환값 보여주기
+		boolean result = control.start();
 	}
 
 }
--- src/com/iu/study4/s2/StudentController.java
+++ src/com/iu/study4/s2/StudentController.java
@@ -1,12 +1,43 @@
 package com.iu.study4.s2;
 
+import java.util.Scanner;
+
 public class StudentController {
 	
-	public void start() {
+	public boolean start() {
 		// 1. 학생 정보 생성 -- makeStudents 호출
 		// 2. 학생 정보 출력 -- SV에서 출력
 		// 3. 프로그램 종료
 		//    -- 2번먼저 누르면 NULL값만 나오니 1번->2번 순서대로 작성하고 프로그램 종료 누를 때까지 돌아가게 생성.
-		
+		// start 메서드 
+		Scanner sc = new Scanner(System.in);
+				
+		boolean check = true;
+				
+		while(check) {
+			System.out.println("학생 정보를 생성하세요!");
+					
+			// SS class 선언
+			StudentService service = new StudentService();
+			// 위 class의 method 선언
+			Student [] count = service.makeStudents();
+					
+			// SC class 선언
+			StudentView view = new StudentView();
+			// 위 class의 method 선언
+			Student [] stview = view.st1(count);
+					
+			// 입력 or 종료 선택
+			System.out.println("다시 입력 : 1 , 프로그램 종료 : 2");
+			int push = sc.nextInt();
+					
+			if(push==2) {
+				System.out.println("프로그램 종료합니다.");
+				check =! check;
+				break;
+			}
+					
+		}
+		return check;
 	}
 }
